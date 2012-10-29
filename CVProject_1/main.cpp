2f4df7ab07b05d358bc4869a19e63e37d65de8a6
Eagle <eagle@Ching-Ches-MacBook-Pro.local>
2012-10-29 11:34:15 -0400

    Initial Commit

---
+++ CVProject_1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CVProject_1
+//
+//  Created by Eagle on 10/29/12.
+//  Copyright (c) 2012 Eagle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
